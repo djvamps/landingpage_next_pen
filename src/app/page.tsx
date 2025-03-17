bba81a6c5731a4ca1691b4821488b995002e45d9
Dany Lopez Zavaleta <dalopeza9@hotmail.com>
2025-03-17 12:29:16 -0500

    Add files via upload
    actualizacion de meta datos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,12 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 
 export const metadata: Metadata = {
   title: "Pengrafic - Agencia de Marketing",
-  description: "Estas en Internt luego existes",
+  description: "Estas en Internt luego existes, somos una agencia integral creativa. Realizamos proyectos de imagen, diseño, marketing digital, audiovisuales.",
+  
+  other: {
+    keywords: "marketing digital, agencia de marketing, diseño gráfico, audiovisuales, branding, publicidad, redes sociales, SEO, contenido digital", // Agrega tus keywords aquí
+  },
+
   // other metadata
 };
 
